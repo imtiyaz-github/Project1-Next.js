f9beaf437f907ca0995d58659e9b8cda6317b2d0
IMTIYAZ M SHIRASANGI <117428054+imtiyaz-github@users.noreply.github.com>
2023-07-22 11:55:39 +0530

    updated

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function MeetupDetails() {
+  return (
+    <div>
+      <MeetupDetails
+        image="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg"
+        title="First Meetup"
+        address="Some Street 5,some city"
+        description="This is a First meetup"
+      />
+    </div>
+  );
+}
+
+export default MeetupDetails;
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,36 @@
+import MeetupList from "../components/meetups/MeetupList";
 
-export default function Home() {
-  return (
-    <div>
-      <h1>Hello Wrold</h1>
-    </div>
-  );
+
+
+const DUMMY_MEETUPS = [
+  {
+    id: "M1",
+    title: "A First Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
+    address: "some address 5,12345 some city",
+    description: "This is a first meetup!",
+  },
+  {
+    id: "M2",
+    title: "A Second Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
+    address: "some address 5,12345 some city",
+    description: "This is a second meetup!",
+  },
+  {
+    id:"M3",
+    title:"A Third Meetup",
+    image:"https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
+    address:"some address 15,some city",
+    description:"This is a Third meetup!",
+  }
+];
+
+
+
+function HomePage() {
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
 }
+export default HomePage;
